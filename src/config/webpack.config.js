44e6855cee9b13c361cae1a55b8250c0316d5ab0
admin <admin>
2019-01-28 18:53:58 -0500

    Added css module to webpack

--- src/config/webpack.config.js
+++ src/config/webpack.config.js
@@ -1,9 +1,18 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "..", "..", "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   }
 };
